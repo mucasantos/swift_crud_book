ed2d73341886ad254fa69085bedab30023fd33ac
Samuel Santos <mucasantos@gmail.com>
2024-04-14 20:56:50 -0300

    Initial Commit

---
+++ MyBooks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBooks
+//
+//  Created by Samuel Santos on 14/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Samuel Santos on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
