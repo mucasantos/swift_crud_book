cad616256237c593b15622a425f2312159392101
Samuel Santos <mucasantos@gmail.com>
2024-04-16 10:01:39 -0300

    Dynamic Sorts and filters

---
+++ MyBooks/BookList.swift
@@ -0,0 +1,18 @@
+//
+//  BookList.swift
+//  MyBooks
+//
+//  Created by Samuel Santos on 16/04/24.
+//
+
+import SwiftUI
+
+struct BookList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookList()
+}
