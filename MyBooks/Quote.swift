28cab238c47de8abd8ddefc893a5b83d10cd418d
Samuel Santos <mucasantos@gmail.com>
2024-04-17 10:56:53 -0300

    Added one to many relationships - View: add Quotes

---
+++ MyBooks/Quote.swift
@@ -0,0 +1,8 @@
+//
+//  Quote.swift
+//  MyBooks
+//
+//  Created by Samuel Santos on 16/04/24.
+//
+
+import Foundation
---
+++ MyBooks/QuotesListView.swift
@@ -0,0 +1,18 @@
+//
+//  QuotesListView.swift
+//  MyBooks
+//
+//  Created by Samuel Santos on 16/04/24.
+//
+
+import SwiftUI
+
+struct QuotesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuotesListView()
+}
